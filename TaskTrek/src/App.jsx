9d5daadcade88e6483878075c2310631dfdc8060
Khalid Shakeel <khalidshakeel46@gmail.com>
2024-04-27 10:07:53 +0530

    TaskTrek Todoapp created with first commit

---
+++ TaskTrek/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className=" bg-emerald-600 h-screen flex justify-center -auto">
+      <span className="bg-black text-white text-5xl font-extrabold p-5 rounded-md shadow-md shadow-fuchsia-200 w-2/3">
+        TASK TREK
+      </span>
+    </div>
+  );
+}
+
+export default App;
